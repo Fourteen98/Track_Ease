42f126499ef7e5e9b272275b4066acfb34d7ff9c
Fourteen98 <emuhiydeen6@gmail.com>
2023-07-21 11:44:09 +0000

    add geocoding

--- parcel/gmaps/geocoding.py
+++ parcel/gmaps/geocoding.py
@@ -1,9 +1,17 @@
+from dotenv import load_dotenv
+import os
 import googlemaps
+
+load_dotenv()
 
 
 class Geocoding(object):
-    def __init__(self, api_key):
-        self.client = googlemaps.Client(key=api_key)
+    def __init__(self):
+        self.client = googlemaps.Client(key=os.getenv('GOOGLE_MAPS_API_KEY'))
 
     def geocode(self, address):
-        pass
+        if address:
+            geocode_result = self.client.geocode(address)
+            if geocode_result:
+                return geocode_result[0]['geometry']['location']
+        return {}
